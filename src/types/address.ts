f154e015ff27b6ae129a036ca9f0b0ea964f8c0b
Hemmachat Paramabuddhi <hemmachat@gmail.com>
2023-10-17 15:01:16 +1100

    test eslint

--- src/types/address.ts
+++ src/types/address.ts
@@ -1,21 +1,21 @@
 export type Address = {
-    streetNumber?: string,
-    streetName?: string,
-    municipality?: string,
-    municipalitySubdivision?: string,
-    countrySecondarySubdivision?: string,
-    countrySubdivision?: string,
-    countrySubdivisionName?: string,
-    countrySubdivisionCode?: string,
-    postalCode?: string,
-    extendedPostalCode?: string,
-    countryCode?: string,
-    country?: string,
-    countryCodeISO3?: string,
-    freeformAddress?: string,
-    localName?: string
+  streetNumber?: string;
+  streetName?: string;
+  municipality?: string;
+  municipalitySubdivision?: string;
+  countrySecondarySubdivision?: string;
+  countrySubdivision?: string;
+  countrySubdivisionName?: string;
+  countrySubdivisionCode?: string;
+  postalCode?: string;
+  extendedPostalCode?: string;
+  countryCode?: string;
+  country?: string;
+  countryCodeISO3?: string;
+  freeformAddress?: string;
+  localName?: string;
 };
 
 export type AddressResult = {
-    placeId?: string
+  placeId?: string;
 } & Address;
