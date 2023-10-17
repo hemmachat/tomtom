1bad18f68b68276abbf9cfc78b77e528db157437
Hemmachat Paramabuddhi <hemmachat@gmail.com>
2023-10-17 15:03:13 +1100

    test

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,21 +1,21 @@
 import { Address } from './address';
 
 type Coordinate = {
-    lat: number,
-    lon: number,
+  lat: number;
+  lon: number;
 };
 
 // Loose data type mapping from the API
 // This is not a full list of the properties, feel free to add anything when you need to use it.
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search#response-body---json
 export type SearchResult = {
-    type?: string,
-    id?: string,
-    score?: number,
-    address?: Address,
-    position?: Coordinate,
-    viewport?: {
-      topLeftPoint?: Coordinate,
-      btmRightPoint?: Coordinate
-    }    
+  type?: string;
+  id?: string;
+  score?: number;
+  address?: Address;
+  position?: Coordinate;
+  viewport?: {
+    topLeftPoint?: Coordinate;
+    btmRightPoint?: Coordinate;
+  };
 };
